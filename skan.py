082ead2fa3a322cf73ecdf0a2c4157fff72b8377
islom <radjapov_islom@mail.ru>
2022-11-23 19:35:40 +0500

    working

---
+++ skan.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+img = cv2.imread("orginal.jpg")
+
+
+cv2.imshow("ima", img)
+cv2.waitKey(0)
+
+
+# TODO: agakgae
--- work1.py
+++ work1.py
@@ -1,6 +1,6 @@
 from time import sleep
 import numpy as np
-from PIL import Image
+
 import cv2
 from helper_funct import drawRectangle
 
@@ -8,7 +8,25 @@ from helper_funct import drawRectangle
 image = cv2.imread('orginal.jpg')
 
 # oq-qora ranga otkashish
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("greyed image", gray_image)
+cv2.waitKey(0)
+
+gray_image = cv2.bilateralFilter(gray_image, 11, 17, 17)
+cv2.imshow("smoothened image", gray_image)
+cv2.waitKey(0)
+
+edged = cv2.Canny(gray_image, 30, 200)
+cv2.imshow("edged image", edged)
+cv2.waitKey(0)
+
+cnts,new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+image1 =image.copy()
+cv2.drawContours(image1,cnts,-1,(0,255,0),3)
+cv2.imshow("contours",image1)
+cv2.waitKey(0)
+
 # savollar = image[340:340+600, 209:209+117]
 # savol_1 = savollar[5:5+25, :]
 # savol_2 = savollar[10:10+25, :]
@@ -19,7 +37,6 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # gray_image = cv2.bilateralFilter(image2, 11, 17, 17)
 # drawRectangle(gray_image,  52, 340, 117, 600 )  # birinchi fan uchun kesib olish
 # drawRectangle(gray_image,  209, 340, 117, 600 )
-
 # show results
 cv2.imshow("ima", image)
 # cv2.imshow("image_1_2", image_1_2)
